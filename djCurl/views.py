f038ed27e036607006276b9bf0784401f48dba76
sid <siddver007@gmail.com>
2016-11-06 21:18:10 +0530

    AAAAA

--- djCurl/views.py
+++ djCurl/views.py
@@ -5,11 +5,11 @@ from subprocess import Popen, PIPE, STDOUT
 def getResult(req, template = 'main.html'):
 	if req.method == 'POST':
 		if req.is_ajax():
-			curl_data = req.POST['curl_text'].strip()
+			curl_data = req.POST['curl_text'].strip()			
 			if curl_data == '' or curl_data is None:
 				a = "!! Empty curl field !!"
 				return HttpResponse(a)
-			elif not curl_data.startswith('curl'):
+			elif not curl_data.startswith('curl') or curl_data[4] != ' ':
 				a = "!! Not a curl command or curl is spelled out wrong !!"
 				return HttpResponse(a)
 			else:	
