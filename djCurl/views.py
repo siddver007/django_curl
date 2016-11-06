cdac26498a02086d9ff836b49c04bb779f6b13d9
sid <siddver007@gmail.com>
2016-11-06 22:44:13 +0530

    AAAAA

--- djCurl/views.py
+++ djCurl/views.py
@@ -5,7 +5,7 @@ from subprocess import Popen, PIPE, STDOUT
 def returnFirstWord(s):
 	i = 0
 	temp = ''
-	while s[i] != ' ' and i != len(s) - 1:
+	while i < len(s) and s[i] != ' ':
 		temp += s[i]
 		i += 1
 	return temp	
@@ -15,10 +15,10 @@ def getResult(req, template = 'main.html'):
 		if req.is_ajax():
 			curl_data = req.POST['curl_text'].strip()			
 			if curl_data == '' or curl_data is None:
-				a = "!! Empty curl field !!"
+				a = "Empty curl field <err1xef2>"
 				return HttpResponse(a)
 			elif returnFirstWord(curl_data) != 'curl':
-				a = "!! Not a curl command or curl is spelled out wrong !!"
+				a = "Not a curl command or curl is spelled out wrong <err1xef2>"
 				return HttpResponse(a)
 			else:	
 				_p = Popen(curl_data + ' -s', shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
