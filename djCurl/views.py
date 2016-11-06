d38c8f17423ef682d3ce401f4020b854c30364dd
sid <siddver007@gmail.com>
2016-11-06 21:46:15 +0530

    AAAAA

--- djCurl/views.py
+++ djCurl/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from subprocess import Popen, PIPE, STDOUT
 
+def returnFirstWord(s):
+	i = 0
+	temp = ''
+	while s[i] != ' ' and i != len(s) - 1:
+		temp += s[i]
+		i += 1
+	return temp	
+
 def getResult(req, template = 'main.html'):
 	if req.method == 'POST':
 		if req.is_ajax():
@@ -9,7 +17,7 @@ def getResult(req, template = 'main.html'):
 			if curl_data == '' or curl_data is None:
 				a = "!! Empty curl field !!"
 				return HttpResponse(a)
-			elif not curl_data.startswith('curl') or curl_data[4] != ' ':
+			elif returnFirstWord(curl_data) != 'curl':
 				a = "!! Not a curl command or curl is spelled out wrong !!"
 				return HttpResponse(a)
 			else:	
