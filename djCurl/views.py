9734f035bd9b7a522355eeb457f4862592b815dd
siddver007 <siddver007@gmail.com>
2018-01-30 04:30:37 -0500

    shell=True -> shell=False; prevents RCE

--- djCurl/views.py
+++ djCurl/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import check_output
+import shlex
 
 def returnFirstWord(s):
 	i = 0
@@ -12,17 +13,21 @@ def returnFirstWord(s):
 
 def getResult(req, template = 'main.html'):
 	if req.method == 'POST':
-		if req.is_ajax():
-			curl_data = req.POST['curl_text'].strip()			
-			if curl_data == '' or curl_data is None:
-				a = "Empty curl field <err1xef2>"
-				return HttpResponse(a)
-			elif returnFirstWord(curl_data) != 'curl':
-				a = "Not a curl command or curl is spelled out wrong <err1xef2>"
-				return HttpResponse(a)
-			else:	
-				_p = Popen(curl_data + ' -s', shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-				output = _p.stdout.read()
-				a = output
-				return HttpResponse(a)
+		try:
+			if req.is_ajax():
+				curl_data = req.POST['curl_text'].strip()			
+				if curl_data == '' or curl_data is None:
+					a = "Empty curl field <err1xef2>"
+					return HttpResponse(a)
+				elif returnFirstWord(curl_data) != 'curl':
+					a = "Not a curl command or curl is spelled out wrong <err1xef2>"
+					return HttpResponse(a)
+				else:	
+					curl_data = shlex.split(curl_data)		
+					_p = check_output(curl_data, shell=False)
+					a = _p
+					return HttpResponse(a)
+		except:
+			a = "Wrong cURL syntax <err1xef2>"	
+			return HttpResponse(a)		
 	return render(req, template)
